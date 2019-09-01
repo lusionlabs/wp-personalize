b0f68083a2de3e456103b8e39061efd4860ea14f
Steven K Word <stevenword@gmail.com>
2019-09-01 13:29:18 -0500

    PHPCS

--- inc/wpPersonlizeEditor.php
+++ inc/wpPersonlizeEditor.php
@@ -12,7 +12,7 @@
 <script type="text/javascript">
 	jQuery(document).ready(function($) {
 		$.ajaxSetup ({cache: false});
-		
+
 		var lastRowID				= 0;
 		var lastScriptCode	= '';
 		var locationArr			= <?php echo json_encode( $locationArr ); ?>;
@@ -24,23 +24,23 @@
 		var php_val 	= '<php\n\n?>';
 		var js_val 		= '<script type="text/javascript">\n\n<script>';
 		js_val = js_val.replace('\n<', '\n</');
-		
+
 		var codeEditor = CodeMirror.fromTextArea(
-			$('#codeEditor').get(0), { 
+			$('#codeEditor').get(0), {
 				lineNumbers: true,
 				matchBrackets: true,
 				mode: "application/x-httpd-php",
 				indentUnit: 2,
 				indentWithTabs: true
 			});
-		
+
 		loadScriptList();
-		
+
 		function loadScriptList() {
 			var postData = {
 				'action'			: 'wpp_load_list'
 			};
-			
+
 			$.post(ajaxurl, postData, function(response) {
 				$('#bodyScriptList .scriptItemRow').remove();
 				if (response.indexOf('{"') >= 0) {
@@ -49,7 +49,7 @@
 
 					var prependHTML = '';
 					var j = 0;
-					
+
 					$.each(parsedData, function(i, obj) {
 						prependHTML += '<tr id="tr-' + j + '" class="scriptItemRow">';
 						prependHTML += '<td>' + obj.title + '</td>';
@@ -75,7 +75,7 @@
 
 			});
 		}
-		
+
 		function setDDValues(ddTitle, ddLocation, ddType, ddArea, buttonAttr) {
 			$('#wpp-title').val(ddTitle);
 			$('#wpp-location').val(ddLocation);
@@ -83,7 +83,7 @@
 			$('#wpp-area').val(ddArea);
 			$('#wpp-submit-button').html($('#wpp-submit-button').attr(buttonAttr));
 		}
-		
+
 		function checkCodeEditorChanges(codeVal) {
 			if (codeVal == '') {
 				return false;
@@ -95,13 +95,13 @@
 				return false;
 			}
 		}
-		
+
 		function loadScript(this_obj) {
 			$('.wpp-section-container').block({message: null});
-			
+
 			var this_id 			= this_obj.id;
 			var script_title 	= $(this_obj).attr('scriptTitle');
-			
+
 			var postData = {
 				'action'	: 'wpp_load_script',
 				'title'		:  script_title
@@ -114,64 +114,64 @@
 
 				codeEditor.setValue(parsedData['code']);
 				lastScriptCode = parsedData['code'];
-				
+
 				setDDValues(script_title, parsedData['location'], parsedData['type'], parsedData['area'], 'updateTxt');
 			});
-			
+
 			$('#tr-' + lastRowID).css({'background-color' : '#FFFFFF'});
 			$('#tr-' + this_id).css({'background-color' : '#EEEEEE'});
-			
+
 			setTimeout(function() {
 				$('.wpp-section-container').unblock();
 			}, 1000);
-			
+
 			lastRowID = this_id;
 		}
-		
-		
+
+
 		$('.btn-submit-code').click(function() {
 			var codeVal = codeEditor.getValue();
-			
+
 			if (codeVal == '') {
 				return;
 			}
-			
+
 			if ($('#wpp-title').val() == '') {
 				$('#wpp-title').focus();
 				return;
 			}
-			
+
 			if ($('#wpp-location').val() == '') {
 				$('#wpp-location').focus();
 				return;
 			}
-			
+
 			if ($('#wpp-type').val() == '') {
 				$('#wpp-type').focus();
 				return;
 			}
-			
+
 			if ($('#wpp-area').val() == '') {
 				$('#wpp-area').focus();
 				return;
 			}
-			
+
 			$('.wpp-section-container').block({message: null});
-			
+
 			var postData = {
 				'action'			: 'wpp_update_script',
-				'codeEditor'	: codeVal, 
+				'codeEditor'	: codeVal,
 				'location'		: $('#wpp-location').val(),
 				'type'				: $('#wpp-type').val(),
 				'area'				: $('#wpp-area').val(),
 				'title'				: $('#wpp-title').val()
 			};
-			
+
 			$.post(ajaxurl, postData, function(response) {
 				response = response.split('{"');
 				response = '{"' + response[1];
 				var parsedData = JSON.parse(response);
-				
+
 				lastScriptCode = codeVal;
 
 				setTimeout(function() {
@@ -181,7 +181,7 @@
 			});
 
 		});
-		
+
 		$('#wpp-type').change(function() {
 			var script_type = $('#wpp-type').val();
 			if (script_type == 'html') {
@@ -194,7 +194,7 @@
 				$('.btn-load-php').trigger("click");
 			}
 		});
-		
+
 		$(document).on('click', '.btn-load-script', function() {
 			var codeVal 	= codeEditor.getValue();
 			var this_obj 	= this;
@@ -206,25 +206,25 @@
 					modal: true,
 					buttons: {
 						"<?php _e( 'Yes', WWP_PLUGIN_LANG_DOMAIN ); ?>": function() {
-							$("#wpp-dialog-confirm").dialog("close");	
+							$("#wpp-dialog-confirm").dialog("close");
 							loadScript(this_obj);
 						},
 						"<?php _e( 'No', WWP_PLUGIN_LANG_DOMAIN ); ?>": function() {
-							$("#wpp-dialog-confirm").dialog("close");	
-							return;			
+							$("#wpp-dialog-confirm").dialog("close");
+							return;
 						}
 					}
 				});
 			} else {
 				loadScript(this_obj);
 			}
-			
+
 		});
-		
+
 		$(document).on('click', '.btn-delete-script', function() {
 			var this_id		= this.id;
 			var this_obj	= this;
-			
+
 			$("#wpp-dialog-confirm-body").html("<?php _e( 'Are you sure?', WWP_PLUGIN_LANG_DOMAIN ); ?>");
 			$("#wpp-dialog-confirm").dialog({
 				resizable: false,
@@ -239,12 +239,12 @@
 							'action'	: 'wpp_delete_script',
 							'title'		:  script_title
 						};
-			
+
 						$.post(ajaxurl, postData, function(response) {
 							response = response.split('{"');
 							response = '{"' + response[1];
 							var parsedData = JSON.parse(response);
-				
+
 							setTimeout(function() {
 								$('.wpp-section-table').unblock();
 								loadScriptList();
@@ -255,17 +255,17 @@
 						});
 					},
 					"<?php _e( 'No', WWP_PLUGIN_LANG_DOMAIN ); ?>": function() {
-						$("#wpp-dialog-confirm").dialog("close");					
+						$("#wpp-dialog-confirm").dialog("close");
 					}
 				}
 			});
-			
+
 		});
-		
+
 		$('.btn-load-html').click(function() {
 			var codeVal 	= codeEditor.getValue();
 			var this_obj 	= this;
-						
+
 			if (checkCodeEditorChanges(codeVal)) {
 				$("#wpp-dialog-confirm-body").html("<?php _e( 'You haven\'t saved your changes, proceed?', WWP_PLUGIN_LANG_DOMAIN ); ?>");
 				$("#wpp-dialog-confirm").dialog({
@@ -274,7 +274,7 @@
 					modal: true,
 					buttons: {
 						"<?php _e( 'Yes', WWP_PLUGIN_LANG_DOMAIN ); ?>": function() {
-							$("#wpp-dialog-confirm").dialog("close");	
+							$("#wpp-dialog-confirm").dialog("close");
 							codeEditor.setValue(html_val);
 							setDDValues('', '', 'html', '', 'createTxt');
 							lastScriptCode = html_val;
@@ -282,8 +282,8 @@
 							lastRowID = 0;
 						},
 						"<?php _e( 'No', WWP_PLUGIN_LANG_DOMAIN ); ?>": function() {
-							$("#wpp-dialog-confirm").dialog("close");	
-							return;			
+							$("#wpp-dialog-confirm").dialog("close");
+							return;
 						}
 					}
 				});
@@ -295,11 +295,11 @@
 				lastRowID = 0;
 			}
 		});
-		
+
 		$('.btn-load-css').click(function() {
 			var codeVal 	= codeEditor.getValue();
 			var this_obj 	= this;
-						
+
 			if (checkCodeEditorChanges(codeVal)) {
 				$("#wpp-dialog-confirm-body").html("<?php _e( 'You haven\'t saved your changes, proceed?', WWP_PLUGIN_LANG_DOMAIN ); ?>");
 				$("#wpp-dialog-confirm").dialog({
@@ -308,7 +308,7 @@
 					modal: true,
 					buttons: {
 						"<?php _e( 'Yes', WWP_PLUGIN_LANG_DOMAIN ); ?>": function() {
-							$("#wpp-dialog-confirm").dialog("close");	
+							$("#wpp-dialog-confirm").dialog("close");
 							codeEditor.setValue(css_val);
 							setDDValues('', '', 'css', '', 'createTxt');
 							lastScriptCode = css_val;
@@ -316,8 +316,8 @@
 							lastRowID = 0;
 						},
 						"<?php _e( 'No', WWP_PLUGIN_LANG_DOMAIN ); ?>": function() {
-							$("#wpp-dialog-confirm").dialog("close");	
-							return;			
+							$("#wpp-dialog-confirm").dialog("close");
+							return;
 						}
 					}
 				});
@@ -329,11 +329,11 @@
 				lastRowID = 0;
 			}
 		});
-		
+
 		$('.btn-load-js').click(function() {
 			var codeVal 	= codeEditor.getValue();
 			var this_obj 	= this;
-						
+
 			if (checkCodeEditorChanges(codeVal)) {
 				$("#wpp-dialog-confirm-body").html("<?php _e( 'You haven\'t saved your changes, proceed?', WWP_PLUGIN_LANG_DOMAIN ); ?>");
 				$("#wpp-dialog-confirm").dialog({
@@ -342,7 +342,7 @@
 					modal: true,
 					buttons: {
 						"<?php _e( 'Yes', WWP_PLUGIN_LANG_DOMAIN ); ?>": function() {
-							$("#wpp-dialog-confirm").dialog("close");	
+							$("#wpp-dialog-confirm").dialog("close");
 							codeEditor.setValue(js_val);
 							setDDValues('', '', 'js', '', 'createTxt');
 							lastScriptCode = js_val;
@@ -350,8 +350,8 @@
 							lastRowID = 0;
 						},
 						"<?php _e( 'No', WWP_PLUGIN_LANG_DOMAIN ); ?>": function() {
-							$("#wpp-dialog-confirm").dialog("close");	
-							return;			
+							$("#wpp-dialog-confirm").dialog("close");
+							return;
 						}
 					}
 				});
@@ -363,11 +363,11 @@
 				lastRowID = 0;
 			}
 		});
-		
+
 		$('.btn-load-php').click(function() {
 			var codeVal 	= codeEditor.getValue();
 			var this_obj 	= this;
-						
+
 			if (checkCodeEditorChanges(codeVal)) {
 				$("#wpp-dialog-confirm-body").html("<?php _e( 'You haven\'t saved your changes, proceed?', WWP_PLUGIN_LANG_DOMAIN ); ?>");
 				$("#wpp-dialog-confirm").dialog({
@@ -376,7 +376,7 @@
 					modal: true,
 					buttons: {
 						"<?php _e( 'Yes', WWP_PLUGIN_LANG_DOMAIN ); ?>": function() {
-							$("#wpp-dialog-confirm").dialog("close");	
+							$("#wpp-dialog-confirm").dialog("close");
 							codeEditor.setValue(php_val);
 							setDDValues('', '', 'php', '', 'createTxt');
 							lastScriptCode = php_val;
@@ -384,8 +384,8 @@
 							lastRowID = 0;
 						},
 						"<?php _e( 'No', WWP_PLUGIN_LANG_DOMAIN ); ?>": function() {
-							$("#wpp-dialog-confirm").dialog("close");	
-							return;			
+							$("#wpp-dialog-confirm").dialog("close");
+							return;
 						}
 					}
 				});
@@ -397,7 +397,7 @@
 				lastRowID = 0;
 			}
 		});
-		
+
 	});
 </script>
 
@@ -415,7 +415,7 @@
 					<textarea id="codeEditor" name="codeEditor"></textarea>
 				</td>
 				<td style="height: 100%;">
-					
+
 					<table class="wpp-sub-section-table" style="height: 90%;">
 						<thead>
 							<tr>
@@ -434,12 +434,12 @@
 							</tr>
 							<tr>
 								<td colspan="5" style="height: 100%; border-bottom: 0px;">
-									
+
 								</td>
 							</tr>
 						</tbody>
 					</table>
-					
+
 				</td>
 			</tr>
 			<tr>
@@ -470,7 +470,7 @@
 					</div>
 				</td>
 				<td>
-					
+
 					<table class="wpp-sub-section-table"  style="height: 100%;">
 						<tbody>
 							<tr>
@@ -508,7 +508,7 @@
 									</select>
 								</td>
 								<td class="wpp-cell-center" style="border-bottom: 0px;">
-									<button class="wpp-btn wpp-btn-sm wpp-btn-primary btn-submit-code" type="button" id="wpp-submit-button" 
+									<button class="wpp-btn wpp-btn-sm wpp-btn-primary btn-submit-code" type="button" id="wpp-submit-button"
 											updateTxt="<?php _e( 'Update', WWP_PLUGIN_LANG_DOMAIN ); ?>" createTxt="<?php _e( 'Create', WWP_PLUGIN_LANG_DOMAIN ); ?>">
 										<?php _e( 'Create', WWP_PLUGIN_LANG_DOMAIN ); ?>
 									</button>
@@ -516,9 +516,9 @@
 							</tr>
 						</tbody>
 					</table>
-					
+
 				</td>
-			</tr>			
+			</tr>
 		</tbody>
 	</table>
 </div>
@@ -526,10 +526,8 @@
 <h3 class="wpp-section-bottom">
 	<section class="wpp-section-bottom-wrap">
 		<div class="wpp-section-bottom-left">
-			2012-<?php echo date( 'Y' ); ?> &#64; <a href="https://ecalon.it" target="_new">Ecalon IT LTD.</a> All rights reserved.
 		</div>
 		<div class="wpp-section-bottom-right">
-			 <a href="mailto:info@ecalon.it" target="_new">Report a Bug</a> |  <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5GB537F64NUDE" target="_new">Donate</a>
 		</div>
 	</section>
 </h3>
